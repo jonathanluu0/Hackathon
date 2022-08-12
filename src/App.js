1ebdd7917cedec7aa25612ce3c108e7f0750bd23
jonathanluu0 <jonathanluu0@gmail.com>
2022-08-12 10:11:42 -0700

    Setup React

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react';
+// import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
+import './App.css';
+
+export default function App(){
+    return(
+        <div>
+            App
+        </div>
+    );
+}
