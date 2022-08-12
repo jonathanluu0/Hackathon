8bccd081d5e0aa9f585a202d51ec6574ea1c7bff
jonathanluu0 <jonathanluu0@gmail.com>
2022-08-12 10:45:16 -0700

    Rerouted front end into client

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react';
 // import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 import './App.css';
-
 export default function App(){
     return(
         <div>
