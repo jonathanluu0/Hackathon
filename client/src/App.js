ab1b442615b1f204c5958e50d0241c008540e982
jonathanluu0 <jonathanluu0@gmail.com>
2022-08-13 01:21:04 -0700

    Card component development

--- client/src/App.js
+++ client/src/App.js
@@ -14,17 +14,26 @@ const cardImages = [
 export default function App() {
     const [cards, setCards] = useState([]);
     const [turns, setTurns] = useState(0);
+
     //shuffle cards when new game button is clicked
     const shuffleCards = () => {
         const shuffledCards = [...cardImages, ...cardImages]
             //if Math.random < 0, order of 2 cards stay the same
             //if Math.random > 0, order of 2 cards are randomized
             .sort(() => Math.random() - 0.5)
-            .map((card) => ({ ...card, id: Math.random() }));
+            .map((card) => ({ ...card, id: Math.random(), state: false }));
 
         setCards(shuffledCards);
         setTurns(0);
         console.log(cards, turns);
+    }
+
+    //handles the flipping of the card, and the choice the user has made
+    const handleChoice = (card) => {
+        //Suppose to flip the card, and fade the card to opacity 0, after which the property will then be disabled.
+        //If not cover.png (clicked on the position of invisible card) then do nothing.
+        card = true;
+        console.log("After card state: " + card);
     }
 
     return (
@@ -36,8 +45,17 @@ export default function App() {
                 <button onClick={shuffleCards}>New Game</button>
             </div>
             <div className='board'>
+                {/* Maps each individual card from the cards array
+                    making them a single card component that handles each individual card
+                    Code for each card component can be found in SingleCard.js
+                */}
                 {cards.map(card => (
-                    <SingleCard key={card.id} card={card} />
+                    <SingleCard 
+                        key={card.id} 
+                        card={card}
+                        handleChoice = {handleChoice}
+                        flipped = {card.state}
+                    />
                 ))}
             </div>
         </div>
--- client/src/components/SingleCard.js
+++ client/src/components/SingleCard.js
@@ -1,11 +1,30 @@
 import './SingleCard.css';
 
-export default function SingleCard({ card }) {
+export default function SingleCard({ card, handleChoice, flipped }) {
+
+    const handleClick = ()=>{
+        console.log("Before card state:" + flipped);
+        handleChoice(this.card.state);
+        console.log("After card state: " + flipped);
+    }
+
     return (
-        <div className='card' key={card.id}>
-            <div>
-                <img className='front' src={card.src} alt='card front' />
-                <img className='back' src='cardBack' alt='card back' />
+        <div className='card'>
+            <div className = {flipped ? "flipped" : ""}>
+                {/* Image of the front of the card */}
+                <img 
+                    className='front' 
+                    src={card.src} 
+                    alt='card front' 
+                />
+
+                {/* Image of the back of the card and what happens when it is clicked */}
+                <img 
+                    className='back' 
+                    src='cardBack' 
+                    onClick={handleClick}
+                    alt='card back' 
+                />
             </div>
         </div>
     )
